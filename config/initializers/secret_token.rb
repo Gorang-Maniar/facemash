4a5fcab43722a484c0271946e75a348093096a29
Gorang-Maniar <gorangmaniar510@gmail.com>
2015-02-14 02:59:01 +0530

    facemash

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facemash::Application.config.secret_token = 'df733f2b2107fcd31cd448a45c2a8dfc37e52d3454df73aa5377a728b9873636cb300ddffce94839e4b6275b4ea2c250c5c495c99f2d38220531e2aee210ac69'
